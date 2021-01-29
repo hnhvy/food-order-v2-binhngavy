8f3751cb61cb608665621ea5723cfd4377d5fabd
ntnga030316 <nguyennga16011999@gmail.com>
2021-01-30 00:44:02 +0700

    open_debug with error response

--- order.php
+++ order.php
@@ -116,7 +116,7 @@
 
                     $total = $price * $qty; // total = price x qty 
 
-                    $order_date = date("Y-m-d h:i:s"); //Order DAte
+                    $order_date = date("Y-m-d h:i:sa"); //Order DAte
 
                     $status = "Ordered";  // Ordered, On Delivery, Delivered, Cancelled
 
@@ -147,10 +147,6 @@
                     $res2 = mysqli_query($conn, $sql2);
 
                     //Check whether query executed successfully or not
-                    echo("Debug");
-                    echo($res2);
-                    echo(mysqli_error($conn));
-                    echo("\n");
                     return;
                     if($res2==true)
                     {
@@ -161,7 +157,10 @@
                     else
                     {
                         //Failed to Save Order
-                        $_SESSION['order'] = "<div class='error text-center'>Failed to Order Food.</div>";
+                        $_SESSION['order'] = "
+                        <div class='error text-center'>Failed to Order Food.</div>
+                         + </br> Error: " .mysqli_error($conn);
+                        
                         header('location:'.SITEURL);
                     }
 
